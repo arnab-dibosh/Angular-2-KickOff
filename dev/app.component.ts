a3790d29ced9b28674f4d84c30713bd95d022004
arnab <arnab.dibosh@gmail.com>
2016-08-14 12:40:25 +0600

    Made a list view

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -3,27 +3,30 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'my-app',
     template: `
-          <h3 (click)="onClick()">{{contact.firstName }} {{contact.lastName}}</h3>
-          <input [(ngModel)]="contact.firstName" type="text">
+          <ul>
+            <li *ngFor="#contact of contactList" (click)="onClick(contact)">
+                 {{contact.firstName}} {{contact.lastName}}
+            </li>
+          </ul>
+          <input [(ngModel)]="selectedContact.firstName" type="text">
+          <br>
           <div>
-            Email: <span *ngIf="showDetail === true"> {{contact.email}}</span>
+            Email: {{selectedContact.email}}
             <br>
-            Phone No: <span *ngIf="showDetail === true"> {{contact.phoneNo}}</span>
+            Phone No: {{selectedContact.phoneNo}}
           </div>
     `,
 })
 export class AppComponent {
 
+  public contactList=[
+    {firstName: "Arnab", lastName: "Dibosh", email: "Arnab.dibosh@gmail.com", phoneNo: "01920-485984" },
+    {firstName: "Arif", lastName: "Sultan", email: "Arif.sultan@gmail.com", phoneNo: "01920-123345" },
+    {firstName: "Yamina", lastName: "Taskin", email: "yamina.taskin@gmail.com", phoneNo: "01920-456789" }
+  ];
 
-  public showDetail=false;
-  public contact={
-    firstName: "Arnab",
-    lastName: "Dibosh",
-    email: "Arnab.dibosh@gmail.com",
-    phoneNo: "01920-485984"
-  };
-
-  onClick(){
-    this.showDetail=true;
+  public  selectedContact={};
+  onClick(clickedItem){
+    this.selectedContact=clickedItem;
   }
 }
